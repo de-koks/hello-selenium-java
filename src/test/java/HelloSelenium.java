a2aef4d878a5b5c574615c4376fdb43a1c209223
Denis Kosun <denis.kosun@gmail.com>
2025-07-18 17:43:46 +0300

    update HelloSelenium.java: - add comments - add implicit wait configuration

--- src/test/java/HelloSelenium.java
+++ src/test/java/HelloSelenium.java
@@ -3,22 +3,33 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.time.Duration;
+import java.util.List;
+
 public class HelloSelenium {
     public static void main(String[] args) throws InterruptedException {
 
+        // create webdriver instance
         WebDriver driver = new ChromeDriver();
+        // set implicit wait for 3 seconds
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
+
+        // go to URL
         driver.get("https://www.selenium.dev");
 
+        // find HTML element by class and click it
         WebElement openSearchButton = driver.findElement(By.className("DocSearch"));
         openSearchButton.click();
 
+        // find another element and input a text into it
         WebElement searchInput = driver.findElement(By.className("DocSearch-Input"));
         searchInput.sendKeys("getting started");
 
-        Thread.sleep(2000);
-        WebElement searchSuggestion = driver.findElement(By.xpath("//li[@class='DocSearch-Hit'][1]"));
-        searchSuggestion.click();
+        // find a list of elements and click the 1st of them
+        List<WebElement> searchSuggestions = driver.findElements(By.xpath("//li[@class='DocSearch-Hit']"));
+        searchSuggestions.get(0).click();
 
+        // wait 2 sec just to watch the result
         Thread.sleep(2000);
         driver.quit();
     }
